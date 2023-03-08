1f0fe5c1e783a5acd88f3fec6516a5c8c036e122
Nikita <nikita.jindal@live.com>
2023-03-08 16:26:36 +1100

    BDD Specflow implementation

---
+++ PracticeCode1/PracticeCode1/Features/TMFeature.feature
@@ -0,0 +1,12 @@
+﻿Feature: TMFeature
+
+As TurnUp Portal admin 
+I would like to create,edit and delete time and material records
+so that I can manage employee record successfully
+
+@tag1
+Scenario: Create time and material record with valid details
+	Given I logged into turnUp portal successfully
+	When I navigate to Time and Material page
+	And I create new Time and Material record
+	Then The record should be created successfully
---
+++ PracticeCode1/PracticeCode1/StepDefinitions/TMFeatureStepDefinitions.cs
@@ -0,0 +1,33 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace PracticeCode1.StepDefinitions
+{
+    [Binding]
+    public class TMFeatureStepDefinitions
+    {
+        [Given(@"I logged into turnUp portal successfully")]
+        public void GivenILoggedIntoTurnUpPortalSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I navigate to Time and Material page")]
+        public void WhenINavigateToTimeAndMaterialPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"I create new Time and Material record")]
+        public void WhenICreateNewTimeAndMaterialRecord()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"The record should be created successfully")]
+        public void ThenTheRecordShouldBeCreatedSuccessfully()
+        {
+            throw new PendingStepException();
+        }
+    }
+}
